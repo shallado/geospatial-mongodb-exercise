ec21175463708d04c36b7cf76f59383a7063c37c
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-30 11:59:24 -0700

    Add geospatial index and practice finding locations near a certain proximity

--- main.js
+++ main.js
@@ -6,9 +6,30 @@ db.places.insertOne({
   name: 'California Academy of Sciences',
   location: {
     type: 'Point',
-    coordinates: [-122,4724356, 37.7672544]
+    coordinates: [-122.4724356, 37.7672544]
   }
 });
+
+// ------------- Adding a GeoSpatial Index to Track the Distance ---------------
+// create geospatial index with the field location
+db.places.createIndex({
+  location: '2dsphere'
+});
+
+// find locations that are between 10 and 500 meters from this given location -122.471114, 37.771104
+db.places.find({
+  location: {
+    $near: {
+      $geometry: {
+        type: 'Point',
+        coordinates: [-122.471114, 37.771104],
+        $maxDistance: 10,
+        $maxDistance: 500
+      }
+    }
+  }
+});
+
 
 
 
